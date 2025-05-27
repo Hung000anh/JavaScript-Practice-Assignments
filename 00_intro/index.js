413d444e806f3007dc633a215ff1d03c31ed4233
AnhNguyen <hung000anh@gmail.com>
2025-05-27 09:22:16 +0700

    Remove introductory module JavaScript file

--- 00_intro/index.js
+++
@@ -1,8 +0,0 @@
-console.log("Welcome to the Introductory Module!");
-
-window.alert('This is alert message from the Introductory Module!');
-
-
-document.getElementById("myH1").textContent = "Hello, this is a heading in the Introductory Module!";
-
-document.getElementById("myP").textContent = "Hello, this is a paragraph in the Introductory Module!";
