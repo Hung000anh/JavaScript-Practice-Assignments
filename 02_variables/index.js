eae46f8f0bcda03197af6447b7a71350fe9dbd5a
AnhNguyen <hung000anh@gmail.com>
2025-05-27 10:15:29 +0700

    Add JavaScript file for variables module with initial content

---
+++ 02_variables/index.js
@@ -0,0 +1,8 @@
+let fullName = "John Doe";
+let age = 30;
+let isStudent = false;
+
+
+document.getElementById("p1").textContent = `Full Name: ${fullName} type: ${typeof fullName} `;
+document.getElementById("p2").textContent = `Age: ${age} type: ${typeof age} `;
+document.getElementById("p3").textContent = `Is Student: ${isStudent} type: ${typeof isStudent} `;
