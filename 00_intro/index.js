b2b3ce6ee40a519708f18cfc5fe065a4a145acc1
AnhNguyen <hung000anh@gmail.com>
2025-05-27 09:15:54 +0700

    Add introductory module JavaScript functionality

---
+++ 00_intro/index.js
@@ -0,0 +1,8 @@
+console.log("Welcome to the Introductory Module!");
+
+window.alert('This is alert message from the Introductory Module!');
+
+
+document.getElementById("myH1").textContent = "Hello, this is a heading in the Introductory Module!";
+
+document.getElementById("myP").textContent = "Hello, this is a paragraph in the Introductory Module!";
