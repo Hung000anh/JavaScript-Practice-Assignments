74a85fac3b038329a2ebfbed11666bc29b5b5c90
AnhNguyen <hung000anh@gmail.com>
2025-05-28 09:09:42 +0700

    Add Math Object examples to demonstrate various mathematical functions

---
+++ 08-math-object/index.js
@@ -0,0 +1,13 @@
+console.log(Math.PI); // 3.141592653589793
+console.log(Math.E); // 2.718281828459045
+console.log(Math.round(4.7)); // 5
+console.log(Math.floor(4.7)); // 4
+console.log(Math.ceil(4.1)); // 5
+console.log(Math.abs(-10)); // 10
+console.log(Math.pow(2, 3)); // 8
+console.log(Math.sqrt(16)); // 4
+console.log(Math.max(1, 2, 3)); // 3
+console.log(Math.min(1, 2, 3)); // 1
+console.log(Math.random()); // Random number between 0 and 1
+console.log(Math.sin(Math.PI / 2)); // 1
+console.log(Math.log(1)); // 0
